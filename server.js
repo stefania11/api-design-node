ce1d04e3b35a4bf96974103bc96af20b7239c69d
System Administrator <root@Octocapis-MacBook-Pro.local>
2016-01-16 18:02:58 -0500

    initial request to root and json data

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+
+// this will send back the index.html file on a GET request to '/'
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/home.html', function(err){
+    if (err){
+      res.status(500).send(err);
+    }
+  })
+});
+
+app.get('/data', function(req,res){
+  res.json(jsonData);
+});
+
+var port = 4000;
+app.listen(port,function(){
+  console.log('listening on http://localhost:', port);
+});
